32355c6f73cac9350aba26f629d5f45f0f8a909e
Avi Avinav <aviavinav2004@gmail.com>
2022-10-30 13:20:21 +0530

    reinitiate tauri app

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  clearScreen: false,
+  server: {
+    strictPort: true,
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
   plugins: [react()],
 });
