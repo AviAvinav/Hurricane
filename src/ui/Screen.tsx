63a85c9bccc676f2439d1f116577833026f20fd8
Avi Avinav <aviavinav2004@gmail.com>
2022-10-30 13:38:08 +0530

    uncommented node-pty

--- src/ui/Screen.tsx
+++ src/ui/Screen.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useRef } from 'react';
 import { Terminal } from 'xterm';
 
-// import pty from 'node-pty';
-// import * as os from 'os';
+import pty from 'node-pty';
+import * as os from 'os';
 
-// const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
-// const ptyProcess = pty.spawn(shell, [], {
-//   name: 'hurricane',
-//   cols: 100,
-//   rows: 20,
-//   cwd: process.env.HOME,
-// });
+const ptyProcess = pty.spawn(shell, [], {
+  name: 'hurricane',
+  cols: 100,
+  rows: 20,
+  cwd: process.env.HOME,
+});
 
 const Screen = () => {
   const terminalContainer = useRef<HTMLDivElement>(null);
